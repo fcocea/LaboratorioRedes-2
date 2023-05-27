0d7bc52dc951be06a7816b52a3450f15e0d8569d
Francisco Cea <fcoceak@gmail.com>
2023-05-27 14:13:58 -0400

    fix: resolve problems processing the request

--- src/libs/utils.cpp
+++ src/libs/utils.cpp
@@ -36,25 +36,25 @@ bool verifyFile(const string &path) {
 map<string, string> parseRequest(const vector<string> &requestLines) {
   map<string, string> requestMap;
   regex headerPattern("([^:]+): *(.*)");
+  // Follow the following format: <method>\s+<path>\s+HTTP/<version>
+  // version like 1.1 / 1
+  // Otherwhise it is a bad rqeuest (400)
   regex requestPattern(
-      "(GET|POST|PUT|DELETE|HEAD|OPTIONS|TRACE|CONNECT) (.*) HTTP/(.*)");
-  if (regex_match(requestLines[0], requestPattern)) {
-    requestMap["Method"] = requestLines[0].substr(0, requestLines[0].find(' '));
-    requestMap["Path"] = requestLines[0].substr(
-        requestLines[0].find(' ') + 1,
-        requestLines[0].rfind(' ') - requestLines[0].find(' ') - 1);
-    requestMap["Version"] =
-        requestLines[0].substr(requestLines[0].rfind(' ') + 1);
-  } else {
+      "([^\\s]+?)\\s+([^\\s]+?)\\s+HTTP\\/(\\d+|(\\d+\\.\\d+))$");
+  if (!regex_match(requestLines[0], requestPattern))
     return requestMap;
-  }
+  const string request = regex_replace(requestLines[0], regex("\\s+"), " ");
+  requestMap["Method"] = request.substr(0, request.find(' '));
+  requestMap["Path"] = request.substr(
+      request.find(' ') + 1, request.rfind(' ') - request.find(' ') - 1);
+  requestMap["Version"] = request.substr(request.rfind(' ') + 1);
   for (const auto &line : requestLines) {
     smatch match;
     if (regex_search(line, match, headerPattern)) {
       requestMap[match[1]] = match[2];
     }
   }
-
+  cout << "Request: " << request << endl;
   return requestMap;
 }
 
